05e815181b71a2078045386bb10fa310d57936e9
riyagoel192 <riyagoel192@gmail.com>
2020-08-17 23:23:48 +0530

    changes to app

--- app/src/main/java/com/example/demoapp/Homepage.java
+++ app/src/main/java/com/example/demoapp/Homepage.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Homepage extends AppCompatActivity {
     Button b1;
     TextView t1,t2,t3,t4;
+    ImageView i1,i2,i3,i4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,10 @@ public class Homepage extends AppCompatActivity {
         t2= (TextView)findViewById(R.id.repair);
         t3= (TextView)findViewById(R.id.food);
         t4= (TextView)findViewById(R.id.education);
+        i1= (ImageView)findViewById(R.id.bs);
+        i2= (ImageView)findViewById(R.id.rm);
+        i3= (ImageView)findViewById(R.id.f);
+        i4= (ImageView)findViewById(R.id.edu);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +38,7 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
-        t1.setOnClickListener(new View.OnClickListener() {
+        i1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i= new Intent(getApplicationContext(),Mobile_Page.class);
@@ -40,7 +46,7 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
-        t2.setOnClickListener(new View.OnClickListener() {
+        i2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i= new Intent(getApplicationContext(),Repair_Page.class);
